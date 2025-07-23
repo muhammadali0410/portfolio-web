ec80513dbc25f49870ba16bc03c6664ea27ad2ad
muhammad-ali055 <ali2412d@aptechorangi.com>
2025-07-23 12:33:17 +0500

    Updated index.html, JS, and CSS files

--- scriptjs/portfolio.js
+++ scriptjs/portfolio.js
@@ -1,4 +1,4 @@
- // Navbar scroll effect
+  // Navbar scroll effect
         window.addEventListener('scroll', function() {
             const navbar = document.querySelector('.navbar');
             if (window.scrollY > 50) {
